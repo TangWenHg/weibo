b5967e3d46cf80c81aabf944efb817405ed17d4e
myhubname <1779129021@qq.com>
2016-07-25 15:05:25 +0800

    初始化项目

---
+++ weibo/weibo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  weibo
+//
+//  Created by 汤文洪 on 16/7/25.
+//  Copyright © 2016年 Tang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
