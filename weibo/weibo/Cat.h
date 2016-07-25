ce09e31e059b29a9d9a0273f5660e8ab7747b7b2
myhubname <1779129021@qq.com>
2016-07-25 15:12:40 +0800

    创建了Cat类

---
+++ weibo/weibo/Cat.h
@@ -0,0 +1,13 @@
+//
+//  Cat.h
+//  weibo
+//
+//  Created by 汤文洪 on 16/7/25.
+//  Copyright © 2016年 Tang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Cat : NSObject
+
+@end
---
+++ weibo/weibo/Cat.m
@@ -0,0 +1,13 @@
+//
+//  Cat.m
+//  weibo
+//
+//  Created by 汤文洪 on 16/7/25.
+//  Copyright © 2016年 Tang. All rights reserved.
+//
+
+#import "Cat.h"
+
+@implementation Cat
+
+@end
